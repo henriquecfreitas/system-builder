0c64301a709581b6d45f09fc26f4aad23bf1b9bc
Henrique Freitas <henriquecfreitas@hotmail.com.br>
2018-05-03 00:03:34 -0300

    Node hello world. First samples created.

---
+++ backend/entityController.js
@@ -0,0 +1,19 @@
+createEntity(){
+	console.log("create");
+};
+
+readEntity(){
+	console.log("read");
+};
+
+updateEntity(){
+	console.log("update");
+};
+
+deleteEntity(){
+	console.log("delete");
+};
+
+listEntities(){
+	console.log("list");
+};
